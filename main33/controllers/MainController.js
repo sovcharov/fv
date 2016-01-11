c0a2b091211ec16cf2503dd014478decdcd9a2a4
yahooserg <seltexinc@gmail.com>
2016-01-11 03:37:04 +0300

    Slight interface changes
    changed styles, sorting and some object properties names

--- main33/controllers/MainController.js
+++ main33/controllers/MainController.js
@@ -5,19 +5,28 @@
         $scope.zero = [];
         $scope.bakeries = [
             {
+                number: 2,
+                address: 'Светлановский 66',
+                date: '11.01',
+                cash: 58002,
+                checks: 212,
+                averageCheck: 199,
+                deepData: []
+            },
+            {
                 number: 1,
-                address: 'Парашютная ул. д. 27 корп. 1',
+                address: 'Парашютная 27',
                 date: '28.08',
-                receipts: 52333,
+                cash: 52333,
                 checks: 122,
                 averageCheck: 99,
                 deepData: []
             },
             {
-                number: 1,
-                address: 'Парашютная ул. д. 27 корп. 1',
-                date: '28.08',
-                receipts: 52333,
+                number: 8,
+                address: 'Энгельса 113',
+                date: '11.01',
+                cash: 582333,
                 checks: 122,
                 averageCheck: 99,
                 deepData: []
