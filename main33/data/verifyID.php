42ebdcc9508da1f4d5093948e5d54f582d4ab625
yahooserg <seltexinc@gmail.com>
2016-01-11 20:03:04 +0300

    ID check
    Working with DB if user is valid to proceed

---
+++ main33/data/verifyID.php
@@ -0,0 +1,15 @@
+<?php
+	require '../../../pdodbconnect.php';
+	$user = $_COOKIE['userID'];
+    $token = $_COOKIE['token'];
+	$q=$dbh->query("call verifyUID($user,$token);");
+	$q->setFetchMode(PDO::FETCH_ASSOC);
+	while ($row = $q->fetch())
+	{
+		$count = (int)$row['outCount'];
+	}
+	$q->closeCursor();
+	$q= null;
+	$dbh = null;
+	echo json_encode($count);
+?>
