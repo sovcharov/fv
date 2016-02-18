44343a6fa6a8297245f025d85467fcbff8c281a6
yahooserg <seltexinc@gmail.com>
2016-02-18 21:18:01 +0300

    Adding table to bakery page

--- modules/BakeryPageController.js
+++ modules/BakeryPageController.js
@@ -1,6 +1,15 @@
 (function () {
     "use strict";
-    angular.module('InvestorPanel').controller("BakeryPageController", function ($scope, $stateParams) {
+    angular.module('InvestorPanel').controller("BakeryPageController", function ($scope, $stateParams, $rootScope) {
+        // $scope.bakeries = $rootScope.bakeries;
+
         $scope.bakeryID = $stateParams.bakeryID || 1;
+        $scope.today = new Date();
+        $scope.yeserday = new Date();
+        $scope.sevenDaysAgo = new Date();
+
+        $scope.dateChanged = function (date) {
+            $scope.today = date;
+        };
     });
 }());
