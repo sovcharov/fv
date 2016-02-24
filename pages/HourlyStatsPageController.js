fded8cacec50a4b90ef2323eb2b9509a2e0b0996
yahooserg <seltexinc@gmail.com>
2016-02-24 19:16:33 +0300

    Hourly stats received from server
    Hourly stats received from server and view prepared

--- pages/HourlyStatsPageController.js
+++ pages/HourlyStatsPageController.js
@@ -1,17 +1,69 @@
 (function () {
     "use strict";
-    angular.module('InvestorPanel').controller("HourlyStatsPageController", function ($scope, $state, $stateParams, $rootScope) {
+    angular.module('InvestorPanel').controller("HourlyStatsPageController", function ($scope, $state, $stateParams, $rootScope, $http) {
         // $scope.bakeries = $rootScope.bakeries;
+        var getData, i;
 
-        $scope.bakeryID = $stateParams.bakeryID || 1;
-        $scope.today = new Date();
+        $scope.bakeryID = $stateParams.bakeryID || "1";
+        $scope.dateToday = new Date();
 
         $scope.dateChanged = function (date) {
-            $scope.today = date;
+            $scope.dateToday = date;
         };
 
         $scope.changeBakery = function (id) {
             $state.go('main.hourlystats', {bakeryID : id});
+        };
+
+        getData = function (bakery, date) {
+            var data = {};
+            data.date = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+            data.store = bakery.id;
+
+
+            // return;
+
+            $http({
+                method: 'POST',
+                url: 'server/getHourlyStats.php',
+                data: data,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function (dataReceived) {
+                bakery.hourlystats = dataReceived;
+
+            }).error(function () {
+
+            });
+        };
+
+        for (i = 0; i < $rootScope.bakeries.length; i = i + 1) {
+            if ($rootScope.bakeries[i].id === parseInt($scope.bakeryID, 10)) {
+                getData($rootScope.bakeries[i], $scope.dateToday);
+            }
+        }
+
+        $scope.getCurrentBakery = function (bakeryID) {
+            for (i = 0; i < $rootScope.bakeries.length; i = i + 1) {
+                if ($rootScope.bakeries[i].id === parseInt(bakeryID, 10)) {
+                    return i;
+                }
+            }
+        };
+
+        $scope.getDateInStringFormat = function (date, offset) {
+            var month = date.getMonth() + 1;
+            if (month < 10) {
+                month = "0" + month;
+            }
+            return date.getFullYear() + '-' + month + '-' + (date.getDate() - offset);
+        };
+
+        $scope.getDateddMM = function (date, offset) {
+            var month = date.getMonth() + 1;
+            if (month < 10) {
+                month = "0" + month;
+            }
+            return (date.getDate() - offset) + '.' + month;
         };
     });
 }());
---
+++ server/getHourlyStats.php
@@ -0,0 +1,27 @@
+<?php
+$postdata = file_get_contents("php://input");
+$request = json_decode($postdata);
+$store =  $request->store;
+$date =  $request->date;
+require '../../dbconnectms.php';
+$msquery="
+    select sum(c.Summa) as total, count(c.Summa) as checks, convert(date,c.DateOperation) as date,DATEPART(hh,c.DateOperation)as time
+    from ChequeHead as c
+    where convert(date,c.DateOperation) > CONVERT(date,getdate()-8)
+    and c.Cash_Code = ".$store."
+    group by convert(date,c.DateOperation),DATEPART(hh,c.DateOperation)
+    order by convert(date,c.DateOperation)
+    ";
+$msqresult = mssql_query($msquery);
+$myArray = [];
+while ($msrow = mssql_fetch_array($msqresult))
+{
+    $myArray[$msrow['time']][$msrow['date']]['cash'] = $msrow['total'];
+    $myArray[$msrow['time']]['time'] = $msrow['time'];
+    $myArray[$msrow['time']][$msrow['date']]['checks'] = $msrow['checks'];
+    $myArray[$msrow['time']][$msrow['date']]['total'] = 0;
+}
+$result = [];
+require '../../dbclosems.php';
+echo json_encode($myArray);
+?>
