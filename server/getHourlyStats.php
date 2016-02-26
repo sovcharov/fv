bd50130d937014de13071474a00253792a5c9ff5
yahooserg <seltexinc@gmail.com>
2016-02-26 20:04:19 +0300

    Adding features to hourlystats
    Sum to hour added and some ui changes

--- server/getHourlyStats.php
+++ server/getHourlyStats.php
@@ -9,17 +9,20 @@ $msquery="
     from ChequeHead as c
     where convert(date,c.DateOperation) > CONVERT(date,getdate()-8)
     and c.Cash_Code = ".$store."
-    group by convert(date,c.DateOperation),DATEPART(hh,c.DateOperation)
-    order by convert(date,c.DateOperation)
+    group by convert(date,c.DateOperation), DATEPART(hh,c.DateOperation)
+    order by DATEPART(hh,c.DateOperation), convert(date,c.DateOperation)
     ";
 $msqresult = mssql_query($msquery);
 $myArray = [];
+$tempArray = [];
 while ($msrow = mssql_fetch_array($msqresult))
 {
     $myArray[$msrow['time']][$msrow['date']]['cash'] = $msrow['total'];
     $myArray[$msrow['time']]['time'] = $msrow['time'];
     $myArray[$msrow['time']][$msrow['date']]['checks'] = $msrow['checks'];
-    $myArray[$msrow['time']][$msrow['date']]['total'] = 0;
+
+    $tempArray[$msrow['date']] = $tempArray[$msrow['date']]+ $msrow['total'];
+    $myArray[$msrow['time']][$msrow['date']]['sum'] = $tempArray[$msrow['date']];
 }
 $result = [];
 require '../../dbclosems.php';
