1c0043bbf6a22c913c786cdee400bb0b706c2314
yahooserg <seltexinc@gmail.com>
2016-07-14 12:32:57 +0300

    FORECAST TEST FUNCTION

--- pages/OrdersPageController.js
+++ pages/OrdersPageController.js
@@ -1,11 +1,11 @@
 (function () {
     "use strict";
     angular.module('InvestorPanel').controller("OrdersPageController", function ($scope, $state, $stateParams, $rootScope, $http) {
-        var getData;
+        var getData, getForecast, getTest;
 
         $scope.bakeryID = $stateParams.bakeryID || "1";
 
-        $rootScope.addLog("Receipts load " + $scope.bakeryID);
+        $rootScope.addLog("ORDERS load " + $scope.bakeryID);
 
         $scope.changeBakery = function (id) {
             $state.go('main.orders', {bakeryID : id});
@@ -22,6 +22,7 @@
             $http.get(url)
                 .success(function (dataReceived) {
                     $scope.order = dataReceived;
+                    // getForecast(dateForUrl, bakery);
                 });
             url = $rootScope.serverAddress + '/api/weather/date/' + dateForUrl;
             $http.get(url)
@@ -30,8 +31,65 @@
                 });
         };
 
+        getForecast = function (dateForUrl, bakery) {
+            var url, i;
+            for (i = 0; i < $scope.order.length; i += 1) {
+                if($scope.order[i].code === 'УТ000000057') {
+                    url = $rootScope.serverAddress + '/api/forecast/store/' + bakery + '/date/' + dateForUrl + '/item/' + $scope.order[i].code;
+                    $http.get(url)
+                        .success(function (dataReceived) {
+                            $scope.forecast = dataReceived;
+                        });
+                }
+            }
+        };
+
+        getTest = function () {
+            var url, i, date, dateForUrl;
+            url = $rootScope.serverAddress + '/api/test/';
+            $http.get(url)
+                .success(function (dataReceived) {
+                    $scope.forecast = dataReceived;
+                    for (i = 0; i < $scope.forecast.length; i += 1) {
+                        date = $scope.forecast[i].date;
+                        dateForUrl = date.slice(0,10);
+                        url = $rootScope.serverAddress + '/api/forecast/store/' + 1 + '/date/' + dateForUrl + '/item/УТ000000023';
+                        $http.get(url)
+                            .success(function (dataReceived) {
+                                var j;
+                                for (j = 0; j < $scope.forecast.length; j += 1) {
+                                    date = $scope.forecast[j].date;
+                                    date = date.slice(0,10);
+                                    if (date === dataReceived.date) {
+                                        $scope.forecast[j].forecast = dataReceived.forecast;
+                                        if($scope.forecast[j].forecast >= $scope.forecast[j].totalDemand) {
+                                            $scope.forecast[j].incomeWouldBe = $scope.forecast[j].totalDemand - ($scope.forecast[j].forecast - $scope.forecast[j].totalDemand);
+                                        } else {
+                                            $scope.forecast[j].incomeWouldBe = $scope.forecast[j].forecast - ($scope.forecast[j].totalDemand - $scope.forecast[j].forecast);
+                                        }
+                                    }
+                                }
+                                $scope.totalIncome = {
+                                    change: 0,
+                                    withoutForecast: 0,
+                                    withForecast: 0
+                                };
+                                for (j = 0; j < $scope.forecast.length; j += 1) {
+                                    $scope.totalIncome.withoutForecast += $scope.forecast[j].income;
+                                    $scope.totalIncome.withForecast += $scope.forecast[j].incomeWouldBe;
+                                }
+                                $scope.totalIncome.averageWithoutForecast = $scope.totalIncome.withoutForecast/$scope.forecast.length;
+                                $scope.totalIncome.averageWithForecast = $scope.totalIncome.withForecast/$scope.forecast.length;
+                                $scope.totalIncome.change = ($scope.totalIncome.withForecast-$scope.totalIncome.withoutForecast)/$scope.totalIncome.withoutForecast * 100;
+                            });
+                    }
+                });
+
+        };
+
         $scope.createOrder = function (bakeryID) {
             getData(bakeryID, $rootScope.ordersDate);
+            getTest();
         };
 
         $scope.dateChanged = function (date) {
