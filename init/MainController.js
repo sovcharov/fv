fd58047186a679793c75e76f27301c9b18642165
yahooserg <seltexinc@gmail.com>
2016-07-18 19:00:45 +0300

    User credential check

--- init/MainController.js
+++ init/MainController.js
@@ -17,6 +17,26 @@
 
         $rootScope.addLog("Main load");
 
+        var userDataCheck = function () {
+            var url = $rootScope.serverAddress + '/api/userDataCheck/' + user.id,
+                daysLeft;
+            $http.get(url).then(function (response) {
+                if (response.data[0]) {
+                    daysLeft = (14 - response.data[0][0].diff);
+                    if (daysLeft < 0) {
+                        $rootScope.addInfoEvent('danger', 'Это ваш последний сеанс', 'или смените пароль сейчас', true);
+                        $rootScope.addInfoEvent('danger', 'временный удален', 'Меню -> Сменить пароль', true);
+                    } else {
+                        $rootScope.addInfoEvent('danger', 'Внимание', 'используется временный пароль', true);
+                        $rootScope.addInfoEvent('danger', 'до блокировки ' + daysLeft + ' дней', 'или Меню -> Сменить пароль', true);
+                    }
+                }
+            }, function () {
+                $rootScope.addInfoEvent('danger', 'Произошла ошибка', 'проверка пользователя');
+            });
+        };
+        userDataCheck();
+
         $rootScope.exitApp = function () {
             $cookies.remove('token');
             $cookies.remove('userID');
