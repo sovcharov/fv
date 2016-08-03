0f4bbc10a349e9ec69ce9279bd467759c76a6a9c
yahooserg <seltexinc@gmail.com>
2016-08-03 18:16:43 +0300

    Users Management get and delete apis added

--- pages/UsersManagementPageController.js
+++ pages/UsersManagementPageController.js
@@ -1,29 +1,51 @@
 /*global angular*/
 (function () {
     "use strict";
-    angular.module('InvestorPanel').controller("UsersManagementPageController", function ($scope, $rootScope, $http) {
+    angular.module('InvestorPanel').controller("UsersManagementPageController", function ($scope, $rootScope, $http, $cookies) {
 
-        $scope.users = [
-            {
-                id: 1,
-                firstName: "Vasiya",
-                lastName: "Pupkin",
-                email: "vasya@pupkin.com"
-            },
-            {
-                id: 2,
-                firstName: "Igor",
-                lastName: "Popkin",
-                email: "igor@pupkin.com"
-            }
-        ];
+        var getUsers,
+            i;
 
-        var url = $rootScope.serverAddress + '/api/users';
-        $http.get(url, { withCredentials: true }).then(function () {
-            $rootScope.addInfoEvent('success', 'Сервер ответил');
-        }, function () {
-            $rootScope.addInfoEvent('danger', 'Произошла ошибка');
-        });
+
+        getUsers = function () {
+            var url = $rootScope.serverAddress + '/api/users/user/' + parseInt($cookies.get('userID'), 10) + '/token/' + parseInt($cookies.get('token'), 10);
+            $http.get(url).then(function (data) {
+                $scope.users = data.data;
+            }, function () {
+                $rootScope.addInfoEvent('danger', 'Произошла ошибка');
+            });
+        };
+        getUsers();
+
+        $scope.deletePressed = function (user) {
+            user.toDelete = true;
+        };
+
+        $scope.deleteCanceled = function (user) {
+            user.toDelete = false;
+        };
+
+        $scope.deleteUser = function (user) {
+            var url = $rootScope.serverAddress + '/api/user/' + parseInt($cookies.get('userID'), 10) + '/token/' + parseInt($cookies.get('token'), 10) + '/userToDelete/' + user.id;
+            $http({
+                method: 'DELETE',
+                url: url
+            }).then(function (data) {
+                if (data.data[0][0].deleted) {
+                    $rootScope.addInfoEvent('success', 'Пользователь удален');
+                    for (i = 0; i < $scope.users.length; i += 1) {
+                        if (user.id === $scope.users[i].id) {
+                            $scope.users.splice(i, 1);
+                        }
+                    }
+                } else {
+                    $rootScope.addInfoEvent('danger', 'Произошла ошибка');
+                }
+            }, function () {
+                $rootScope.addInfoEvent('danger', 'Произошла ошибка');
+                user.toDelete = false;
+            });
+        };
 
     });
 }());
