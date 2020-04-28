4f07210d853cd3cf172564dc2826b75610e8e53f
yahooserg <seltexinc@gmail.com>
2020-04-28 17:58:57 +0700

    test

--- server/verifyID.php
+++ server/verifyID.php
@@ -1,22 +1,14 @@
 <?php
-	// require './../../pdodbconnect.php';
+	// require '../../pdodbconnect.php';
 	require __DIR__ . '/../../pdodbconnect.php';
-		// $user = $_COOKIE['userID'];
-    // $token = $_COOKIE['token'];
-		echo "hello";
-		// echo $user;
-		// echo $token;
-		echo $dbh;
-		?>
-		//
-		// $count = 1;
-	// exit();
-	// $q=$dbh->query("call verifyUID('.$user.','.$token.');");
-	// $q->setFetchMode(PDO::FETCH_ASSOC);
-	// while ($row = $q->fetch())
-	// {
-	// 	$count = (int)$row['outCount'];
-	// }
-	// $q->closeCursor();
-	// $q= null;
-	// $dbh = null;
+	$user = $_COOKIE['userID'];
+    $token = $_COOKIE['token'];
+	$q=$dbh->query("call verifyUID('.$user.','.$token.');");
+	$q->setFetchMode(PDO::FETCH_ASSOC);
+	while ($row = $q->fetch())
+	{
+		$count = (int)$row['outCount'];
+	}
+	$q->closeCursor();
+	$q= null;
+	$dbh = null;
