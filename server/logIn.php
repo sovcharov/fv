e4974c8f946f018d9b2a3bd0279bf7470426e93a
yahooserg <seltexinc@gmail.com>
2020-04-28 20:09:03 +0700

    test login

--- server/logIn.php
+++ server/logIn.php
@@ -9,19 +9,46 @@ if(preg_match($regexEmail,$email) && preg_match($regexPassword,$password)){
     $password = SHA1($password);
     $token = rand(10000,9999999);
     require '../../pdodbconnect.php';
-    $q=$dbh->query("call logIn('$email','$password','.$token.');");
-    $q->setFetchMode(PDO::FETCH_ASSOC);
-    while ($row = $q->fetch())
-    {
-        $result["userID"] = (int)$row['userID'];
-        $result["userType"] = (int)$row['typeID'];
-        $result["firstName"] = $row['firstName'];
-        $result["lastName"] = $row['lastName'];
-        $result["token"] = $token;
-    }
-    $q->closeCursor();
-    $q= null;
-    $dbh = null;
+
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+  	// Check connection
+  	if ($conn->connect_error) {
+  	    die("Connection failed: " . $conn->connect_error);
+  	}
+  	$sql = "call logIn('$email','$password','.$token.');";
+  	$result = $conn->query($sql);
+  	if ($result->num_rows > 0) {
+  	    // output data of each row
+  	    while($row = $result->fetch_assoc()) {
+          $result["userID"] = (int)$row['userID'];
+          $result["userType"] = (int)$row['typeID'];
+          $result["firstName"] = $row['firstName'];
+          $result["lastName"] = $row['lastName'];
+          $result["token"] = $token;
+  	    }
+  	} else {
+  		$count = 0;
+  		// echo "0 results";
+  	}
+  	$conn->close();
+    // 
+    //
+    //
+    // $q=$dbh->query("call logIn('$email','$password','.$token.');");
+    // $q->setFetchMode(PDO::FETCH_ASSOC);
+    // while ($row = $q->fetch())
+    // {
+    //     $result["userID"] = (int)$row['userID'];
+    //     $result["userType"] = (int)$row['typeID'];
+    //     $result["firstName"] = $row['firstName'];
+    //     $result["lastName"] = $row['lastName'];
+    //     $result["token"] = $token;
+    // }
+    // $q->closeCursor();
+    // $q= null;
+    // $dbh = null;
     if ($result) echo json_encode($result);
     else echo false;
 } else echo false;
