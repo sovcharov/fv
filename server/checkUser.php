bf7cf49dd50b5d90dd819ab997af2a00ce0f1621
yahooserg <seltexinc@gmail.com>
2020-04-28 17:43:25 +0700

    test

--- server/checkUser.php
+++ server/checkUser.php
@@ -1,5 +1,5 @@
 <?php
-    // require 'verifyID.php';
+    require 'verifyID.php';
     if ($count){
         echo 1;
     } else echo 0;
--- server/verifyID.php
+++ server/verifyID.php
@@ -2,12 +2,18 @@
 	require '../../pdodbconnect.php';
 	$user = $_COOKIE['userID'];
     $token = $_COOKIE['token'];
-	$q=$dbh->query("call verifyUID('.$user.','.$token.');");
-	$q->setFetchMode(PDO::FETCH_ASSOC);
-	while ($row = $q->fetch())
-	{
-		$count = (int)$row['outCount'];
-	}
-	$q->closeCursor();
-	$q= null;
-	$dbh = null;
+
+		echo $user;
+		echo $token;
+		echo $dbh;
+		$count = 1;
+	// exit();
+	// $q=$dbh->query("call verifyUID('.$user.','.$token.');");
+	// $q->setFetchMode(PDO::FETCH_ASSOC);
+	// while ($row = $q->fetch())
+	// {
+	// 	$count = (int)$row['outCount'];
+	// }
+	// $q->closeCursor();
+	// $q= null;
+	// $dbh = null;
