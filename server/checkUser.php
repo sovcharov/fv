30816ce6cca95615cf4487602e6da155f8a73cad
yahooserg <seltexinc@gmail.com>
2020-04-28 18:15:16 +0700

    test

--- server/checkUser.php
+++ server/checkUser.php
@@ -1,6 +1,22 @@
 <?php
-    require 'verifyID.php';
-    if ($count){
-        echo 1;
-    } else echo 0;
+require __DIR__ . '/../../pdodbconnect.php';
+
+
+try {
+    // $dbh = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
+
+
+    // require 'verifyID.php';
+    // if ($count){
+    //     echo 1;
+    // } else echo 0;
 ?>
