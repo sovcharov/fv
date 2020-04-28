bd0914dcf7abdd37155cd1031ba0eebc61769e93
yahooserg <seltexinc@gmail.com>
2020-04-28 19:21:02 +0700

    test object

--- server/checkUser.php
+++ server/checkUser.php
@@ -1,32 +1,6 @@
 <?php
-require __DIR__ . '/../../pdodbconnect.php';
-
-
-try {
-  $user = 1;//$_COOKIE['userID'];
-    $token = 3151463;//$_COOKIE['token'];
-
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("select * from users");
-      $stmt->execute();
-
-      // set the resulting array to associative
-      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-      foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-          echo $v;
-      }
-
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
-    $dbh = null;
-
-
-
-    // require 'verifyID.php';
-    // if ($count){
-    //     echo 1;
-    // } else echo 0;
+    require 'verifyID.php';
+    if ($count){
+        echo 1;
+    } else echo 0;
 ?>
--- server/verifyID.php
+++ server/verifyID.php
@@ -10,5 +10,16 @@
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 	}
-	echo "Connected successfully";
+	$sql = "SELECT id, firstname, lastname FROM users";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+	    }
+	} else {
+	    echo "0 results";
+	}
+	$conn->close();
 ?>
