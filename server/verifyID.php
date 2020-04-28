efb58640173db721abe84898cf380b3caf40fa44
yahooserg <seltexinc@gmail.com>
2020-04-28 18:02:09 +0700

    test

--- server/verifyID.php
+++ server/verifyID.php
@@ -1,8 +1,8 @@
 <?php
 	// require '../../pdodbconnect.php';
-	require __DIR__ . '/../../pdodbconnect.php';
-	$user = $_COOKIE['userID'];
-    $token = $_COOKIE['token'];
+	// require __DIR__ . '/../../pdodbconnect.php';
+	$user = 1;//$_COOKIE['userID'];
+    $token = 3151463;//$_COOKIE['token'];
 	$q=$dbh->query("call verifyUID('.$user.','.$token.');");
 	$q->setFetchMode(PDO::FETCH_ASSOC);
 	while ($row = $q->fetch())
