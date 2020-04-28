de148207c77a52fa39e9f5b8f671609c17215638
yahooserg <seltexinc@gmail.com>
2020-04-28 20:52:59 +0700

    test

--- server/logIn.php
+++ server/logIn.php
@@ -20,6 +20,7 @@ if(preg_match($regexEmail,$email) && preg_match($regexPassword,$password2)){
     echo $token;
 
     $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn->set_charset("utf8");
 
   	// Check connection
   	if ($conn->connect_error) {
