8ad7f1bf1b053a04d8a4f5dda002a18e14333bdf
yahooserg <seltexinc@gmail.com>
2020-04-28 21:03:54 +0700

    login fix

--- server/logIn.php
+++ server/logIn.php
@@ -1,21 +1,20 @@
 <?php
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
-// $email = $request->email;
-// $password2 = $request->password;
-require __DIR__ . '/../../pdodbconnect.php';
+$email = $request->email;
+$password2 = $request->password;
 
 
 $regexEmail = '/^[A-Za-z0-9]+((([.\-_])[A-Za-z0-9]+)?)*@[A-Za-z0-9]+((([.\-_])[A-Za-z0-9]+)?)*\.[A-Za-z]{2,4}$/';
 $regexPassword = '/^[A-Za-z0-9.\-_*$]{5,}$/';
 if(preg_match($regexEmail,$email) && preg_match($regexPassword,$password2)){
-  // require __DIR__ . '/../../pdodbconnect.php';
-    $password2 = SHA1($password);
+  require __DIR__ . '/../../pdodbconnect.php';
+    $password2 = SHA1($password2);
     $token = rand(10000,9999999);
 
     $conn = new mysqli($servername, $username, $password, $dbname);
     $conn->set_charset("utf8");
-    echo "jhklj";
+
   	// Check connection
   	if ($conn->connect_error) {
   	    die("Connection failed: " . $conn->connect_error);
@@ -31,8 +30,8 @@ if(preg_match($regexEmail,$email) && preg_match($regexPassword,$password2)){
           $res["lastName"] = $row['lastName'];
           $res["token"] = $token;
   	    }
-        echo json_encode($res);
-        // else echo false;
+        if ($res) echo json_encode($res);
+        else echo false;
   	} else {
   		echo false;
   		// echo "0 results";
