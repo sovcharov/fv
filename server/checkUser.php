8712bb51a5ab0fb0927e6a902129595d18dc2c4d
yahooserg <seltexinc@gmail.com>
2020-04-28 18:30:31 +0700

    test pdo

--- server/checkUser.php
+++ server/checkUser.php
@@ -8,9 +8,9 @@ try {
 
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $dbh->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
-    $stmt = $dbh->prepare("Call verifyUID(:user, :token)");
-    $stmt->bindParam(':user', $user);
-    $stmt->bindParam(':token', $token);
+    $stmt = $dbh->prepare("Call verifyUID(?, ?)");
+    $stmt->bindParam(1, $user);
+    $stmt->bindParam(2, $token);
     $stmt->execute();
     $rs = $stmt->execute();
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
