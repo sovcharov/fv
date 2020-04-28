45ae6a6579ea074dfa114e452c826edcbb3e3e0d
yahooserg <seltexinc@gmail.com>
2020-04-28 18:42:24 +0700

    test

--- server/checkUser.php
+++ server/checkUser.php
@@ -8,9 +8,9 @@ try {
 
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $stmt = $dbh->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
-    $stmt = $dbh->prepare("Call verifyUID(?, ?)");
-    $stmt->bindParam(1, $user);
-    $stmt->bindParam(2, $token);
+    $stmt = $dbh->prepare("CALL verifyUID(:user, :token)");
+    $stmt->bindParam(':user', $user);
+    $stmt->bindParam(':token', $token);
     $stmt->execute();
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
     foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
@@ -23,11 +23,8 @@ catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
     }
-
     $dbh = null;
-
 ?>
-
 
 
     // require 'verifyID.php';
