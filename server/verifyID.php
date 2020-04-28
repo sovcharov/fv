859da7812bd864cf3739c611a2b9c7abdcc240fd
yahooserg <seltexinc@gmail.com>
2020-04-28 19:39:29 +0700

    test

--- server/verifyID.php
+++ server/verifyID.php
@@ -16,10 +16,10 @@
 	if ($result->num_rows > 0) {
 	    // output data of each row
 	    while($row = $result->fetch_assoc()) {
-	        echo $row["outCount"];
+	        $count = $row["outCount"];
 	    }
 	} else {
-	    echo "0 results";
+		$count = $row["outCount"];
+		// echo "0 results";
 	}
 	$conn->close();
-?>
