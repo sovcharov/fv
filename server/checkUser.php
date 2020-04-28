768d56dae953eddc9f4476f6e9443da9512530d3
yahooserg <seltexinc@gmail.com>
2020-04-28 18:52:15 +0700

    test

--- server/checkUser.php
+++ server/checkUser.php
@@ -6,10 +6,15 @@ try {
   $user = 1;//$_COOKIE['userID'];
     $token = 3151463;//$_COOKIE['token'];
 
-    // $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    $stmt = $conn->prepare("Call verifyUID(1,3151463)");
+      $stmt->execute();
 
+      // set the resulting array to associative
+      $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+      foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+          echo $v;
+      }
 
     }
 catch(PDOException $e)
@@ -17,7 +22,7 @@ catch(PDOException $e)
     echo "Connection failed: " . $e->getMessage();
     }
     $dbh = null;
-?>
+
 
 
     // require 'verifyID.php';
