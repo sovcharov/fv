c97a21640312a9c8a940d80ff8c28be0f848999b
yahooserg <seltexinc@gmail.com>
2020-04-28 18:44:58 +0700

    test

--- server/checkUser.php
+++ server/checkUser.php
@@ -6,16 +6,9 @@ try {
   $user = 1;//$_COOKIE['userID'];
     $token = 3151463;//$_COOKIE['token'];
 
+    // $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $dbh->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (:firstname, :lastname, :email)");
-    $stmt = $dbh->prepare("CALL verifyUID(:user, :token)");
-    $stmt->bindParam(':user', $user);
-    $stmt->bindParam(':token', $token);
-    $stmt->execute();
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-        echo $v;
-    }
+    echo "Connected successfully";
 
 
     }
